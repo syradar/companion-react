2b39e41b2e6ec273420f47cc5b36f2e8e946d60c
David Malmström <syntex@outlook.com>
2020-07-24 12:44:28 +0200

    add generic table component

---
+++ src/components/table.tsx
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw, { css } from 'twin.macro';
+import { FunctionComponent } from 'react';
+
+export interface TableCell {
+  value: string | number;
+  alignRight: boolean;
+}
+
+export interface TableRow {
+  isHeader: boolean;
+  cells: TableCell[];
+}
+
+interface TableProps {
+  rows: TableRow[];
+}
+
+export const Table: FunctionComponent<TableProps> = ({ rows }: TableProps) => {
+  return (
+    <div className="prose" tw="mb-4">
+      <table>
+        <thead>
+          <tr>
+            {rows
+              .filter(r => r.isHeader)
+              .map(r =>
+                r.cells.map(c => (
+                  <th css={[c.alignRight && tw`text-right`]}>{c.value}</th>
+                )),
+              )}
+          </tr>
+        </thead>
+        <tbody>
+          {rows
+            .filter(r => !r.isHeader)
+            .map(r => (
+              <tr>
+                {r.cells.map(c => (
+                  <td css={[c.alignRight && tw`text-right`]}>{c.value}</td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
